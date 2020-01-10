7b6678767f81414f9c3283a080b36292ca2f1f65
HAN HYUNSEOB <hanhyunseob@gmail.com>
2020-01-10 19:40:41 +0900

    2020.01.10
    about 페이지 마지막 article 효과가 저희 오뉴 모바일 리뉴얼 인덱스 2번쨰 article이랑 동일한데 효과가 들어가지 않습니다.

--- js/about.js
+++ js/about.js
@@ -1,75 +1,61 @@
 $(document).ready(function () {
-    // cnt1
-    var scrollY = 0; //윈도우 상단좌표값
-    var timer = 0; //한번만 실행시키기 위한 변수
-    // .cnt2
-    var j = 0;
-    setInterval(function () {
-        j++;
-        console.log(j);
-        $(".cnt1").each(function() {
-            if (j > 2) $('.after').addClass('bgchange');
-            else $('.after').removeClass('bgchange');
-        });
-    }, 1000);
-
-    $(window).on("scroll", function () {
-        clearTimeout(timer);
-
-        timer = setTimeout(function () {
-            scrollY = $(this).scrollTop();
-            console.log(scrollY);
-
-            $(".fade").each(function () {
-                if (scrollY > $(this).offset().top - 600) $(this).stop()
-                    .animate({
-                        opacity: 1,
-                        '-ms-filter': '"progid:DXImageTransform.Microsoft.Alpha(Opacity=100)"',
-                        top: 0
-                    });
-                else $(this).stop().animate({
-                    opacity: 0,
-                    '-ms-filter': '"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)"',
-                    top: 40
-                });
-            });
-        }, 100);
+  // cnt1
+  var scrollY = 0; //윈도우 상단좌표값
+  var timer = 0; //한번만 실행시키기 위한 변수
+  // .cnt2
+  var j = 0;
+  setInterval(function () {
+    j++;
+    console.log(j);
+    $(".cnt1").each(function () {
+      if (j > 2) $('.after').addClass('bgchange');
+      else $('.after').removeClass('bgchange');
     });
-    
-    /* Initialize Swiper */
-        var galleryThumbs = new Swiper('.gallery-thumbs', {
-            spaceBetween: 10,
-            slidesPerView: 4,
-            loop: true,
-            freeMode: true,
-            loopedSlides: 5, //looped slides should be the same
-            watchSlidesVisibility: true,
-            watchSlidesProgress: true,
-          });
-          var galleryTop = new Swiper('.gallery-top', {
-            spaceBetween: 10,
-            loop:true,
-            loopedSlides: 5, //looped slides should be the same
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-            thumbs: {
-              swiper: galleryThumbs,
-            },
-          });
+  }, 1000);
 
-        var swiper = new Swiper('.cnt2 .swiper-container', {
-            effect: 'flip',
-            grabCursor: true,
-            pagination: {
-              el: '.swiper-pagination',
-            },
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          });
+  $(window).on("scroll", function () {
+    clearTimeout(timer);
 
-      
+    timer = setTimeout(function () {
+      scrollY = $(this).scrollTop();
+      console.log(scrollY);
+      if (scrollY > 1600) $('.cnt4').addClass('on');
+    });
+      /* Initialize Swiper */
+      var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        loop: true,
+        freeMode: true,
+        loopedSlides: 5, //looped slides should be the same
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+      var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        loop: true,
+        loopedSlides: 5, //looped slides should be the same
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+          swiper: galleryThumbs,
+        },
+      });
+
+      var swiper = new Swiper('.cnt2 .swiper-container', {
+        effect: 'flip',
+        grabCursor: true,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+    });
+  
 });
